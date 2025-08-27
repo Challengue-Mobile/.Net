2df7a282314d29178f40bba46a15bb2da5b8476c
fmelods <felipemelodesousa4@gmail.com>
2025-08-26 23:13:38 -0300

    Corrigindo erro do jsonrequired

--- Models/Beacon.cs
+++ Models/Beacon.cs
@@ -1,21 +1,28 @@
-using System.ComponentModel.DataAnnotations; // ← ADICIONADO
+using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization; // ← ADICIONADO
+
 
 namespace API_.Net.Models
 {
     public class Beacon
     {
         [Required] // ← CORRIGIDO - Issue L5
+        [JsonRequired]
         public int ID_BEACON { get; set; }
         
+        [Required] 
         public string UUID { get; set; } = string.Empty;
         
         [Required] // ← CORRIGIDO - Issue L7
+        [JsonRequired]
         public int BATERIA { get; set; }
         
         [Required] // ← CORRIGIDO - Issue L8
+        [JsonRequired]
         public int ID_MOTO { get; set; }
         
         [Required] // ← CORRIGIDO - Issue L9
+        [JsonRequired]
         public int ID_MODELO_BEACON { get; set; }
         
         // Propriedades de navegação
